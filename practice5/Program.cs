bec64b4014bd68c99c94f33c806f138200c553df
SirGoogle <SirGoogle@DESKTOP-V6QGN9V>
2023-10-31 14:43:41 +0330

    Add project files.

---
+++ practice5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace practice5
+{
+    class Program
+    {
+        //MohammadReza Fatahi
+        //tamrin5
+        static void Main(string[] args)
+        {
+            first:
+            Console.WriteLine("adad mored nazar ra baraye mohasebe factoriel an vared konid :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 1;
+            for (int i = 1; i <= a; i++)
+                b *= i;
+            Console.WriteLine(b);
+            Console.WriteLine("aya mikhay edame bedi?! (y/n)");
+            string x = Console.ReadLine();
+            if (x == "y")
+            {
+                goto first;
+            }
+            else
+            {
+                Console.WriteLine("khodafez");
+            }
+        }
+    }
+}
